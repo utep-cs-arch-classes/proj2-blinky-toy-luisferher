c76a965392b806a1f62d134cf851a80381e2e282
luisferher <lfhernandez11@miners.utep.edu>
2021-04-10 13:40:06 -0600

    Added switches, buzzer and p1_interrup_handler files to project and modified Makefile

--- project/wdInterruptHandler.c
+++ project/wdInterruptHandler.c
@@ -5,12 +5,13 @@ void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static char blink_count = 0;
   static int state_count = 0;
-  if (++blink_count == 2) {
-    dimming();
+  if (++blink_count == 125) {
+    //dimming();
+    simple_state();
     blink_count = 0;
   }
   if(++state_count == 325){
-    change_dimming();
+    //change_dimming();
     state_count = 0;
   }
 }
